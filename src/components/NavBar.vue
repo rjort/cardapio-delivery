46c1bf0b962f7d06ee1fdf2cb364267b32025801
Reuter Junior <reuter_junior@hotmail.com>
2020-02-02 09:06:18 -0300

    ...

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+      <v-app-bar app color="primary" dark>
+        <span class="mr-2">Latest Release</span>
+      </v-app-bar>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style>
+
+</style>
