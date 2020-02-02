c3e35dfb8ca802bc49c6341104ecedd09cced33d
Reuter Junior <reuter_junior@hotmail.com>
2020-02-02 08:55:46 -0300

    refazendo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <v-app>
-		<Navbar />
-    <Content />
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Navbar from "@/components/Navbar";
-import Content from "@/components/Content";
-
-export default {
-  name: "app",
-  components: {
-		Navbar,
-    Content
-  }
-};
-</script>
-
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import "@babel/polyfill";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
