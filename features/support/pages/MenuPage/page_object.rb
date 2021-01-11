1af6c9b394c8b83b109dfb4c05c122ee942b3ee6
Reuter Junior <reuter.regis@compasso.com.br>
2021-01-11 17:59:49 -0300

    2021/01/11: Adicionado novas features e refatorado alguns metodos
    Foi adicionado novos metodos para clicar no botao de finalizar pedido no
    card do Carrinho e melhorado funcionamento do
    select_options_without_aditional onde antes nao eram reconhecidos os
    radio buttons quando implementado 'MenuPage::title_cart'.
    
    O bug ainda persiste porem nao ocorre com muita frequencia, acho que
    seja codigo no front-end mal organizado ou com erro de logica.

--- features/support/pages/MenuPage/page_object.rb
+++ features/support/pages/MenuPage/page_object.rb
@@ -20,27 +20,54 @@ module CardapioDelivery
       def select_all_options()
       end
 
+      def button_add_cart()
+        within('#field-order') do
+          find('#add-order').click
+        end
+      end
+
+      def whatsapp_title_check
+        find('#top-of-page')
+      end
+
+      def button_finish_order()
+        within('#btn-actions') do
+          find('#finish').click
+        end
+      end
+
+      # Usado para fazer a verificacao se um pedido foi adicionado ao carrinho
+      def id_order
+        find('#order-id')
+      end
+
+      def check_id_order
+        yield(id_order()) if block_given?
+      end
+
       # Acao para selecionar todas as opcoes menos as adicionais
       #
       # v:1.0 de funcionamento
       # Verificar o codigo e depois tentar melhorar
       def select_options_without_aditional()
-        # clicks_values = {}
+        clicks_values = {}
         create_rand_values()
-        choose('', option: @value[:tamanho], allow_label_click: true)
-        choose('', option: @value[:massa], allow_label_click: true)
-        choose('', option: @value[:molho], allow_label_click: true)
-        choose('', option: @value[:proteina], allow_label_click: true)
-
+        # sleep 2
+        # binding.pry
+        choose('tamanho', name: 'tamanho', option: @value[:tamanho], allow_label_click: true)
+        choose('massa', name: 'massa', option: @value[:massa], allow_label_click: true)
+        choose('molho', name: 'molho', option: @value[:molho], allow_label_click: true)
+        choose('proteina', name: 'proteina', option: @value[:proteina], allow_label_click: true)
+        
         # TODO: Verificar uma maneira de chegar os campos das labels para checar o click
         # e passa-lo pelo yield() para fazer uma verificacao com expect().to be_checked
         # Exemplo
-        #
+        # #
         # clicks_values[:tamanho] = find_field(@value[:tamanho], checked: true)
         # clicks_values[:massa] = find_field(@value[:massa], checked: true)
         # clicks_values[:molho] = find_field(@value[:molho], checked: true)
         # clicks_values[:proteina] = find_field(@value[:proteina], checked: true)
-        # yield(clicks_values)
+        # yield(clicks_values) if block_given?
         # # # # # # VERFICAR NO CARDAPIO_STEPS CONTINUACAO DO CODIGO # # # # # # 
       end
 
